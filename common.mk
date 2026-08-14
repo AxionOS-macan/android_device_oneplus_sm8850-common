ee7033cfff7e69d05e846498e59a47f46f55bce7
blazerpaul15 <blazerpaul15@gmail.com>
2026-08-14 19:37:57 +0900

    sm8850-common: Introduce OReality Audio

--- common.mk
+++ common.mk
@@ -277,6 +277,9 @@ PRODUCT_PACKAGES += \
     vendor.oplus.hardware.osense.client-service \
     vendor.oplus.hardware.performance-service
 
+# OReality Audio
+$(call inherit-product-if-exists, vendor/oneplus/oreality/oreality-audio.mk)
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
